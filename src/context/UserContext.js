e046cd96190c1818e7641e1cb2ddecabd3eefef2
Allison Ause <allison.ause@gmail.com>
2022-09-28 12:00:57 -0700

    added UserContext.js content to provide globally callable user state

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,1 +1,14 @@
-// TODO -- create the user provider!
+import { createContext, useState } from 'react';
+import { getUser } from '../services/auth';
+
+// Call createContext to create UserContext that's passable
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const currentUser = getUser();
+  const [user, setUser] = useState(currentUser);
+
+  return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
+};
+
+export { UserContext, UserProvider };
